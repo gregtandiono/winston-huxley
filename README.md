582055f07ec0aac62105292718e1f61433936179
gregtandiono <gregtandiono@gmail.com>
2013-11-04 02:41:57 -0800

    push from local dev. env to Repo

---
+++ README.md
@@ -0,0 +1,6 @@
+#The New Winston Huxley Site
+##A repo for my new Company Site
+
+Tools used: Node, Wintersmith, Vex
+
+Author: Gregory Tandiono
---
+++ js/app.js
@@ -0,0 +1,14 @@
+(function() {
+				'use strict';
+
+				var target = document.getElementById('target'),
+					typer = new Typer(target);
+
+				typer.write('Welcome to Winston Huxley. We create Rich Visual Experiences.');
+			})();
+
+$('.theContent').click(function(){
+vex.open({
+content:'<div class="uk-container uk-container-center"><img src="img/logo-badge.svg"><br><br><br><a href="">Work</a><br><br><br><a href="">Services</a><br><br><br><a href="">About</a><br><br><br><a href="">Contact</a><br><br><br></div>',
+});
+});
