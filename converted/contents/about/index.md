c3da3c243c6f1aee3cd19577b3f079a825308265
gregtandiono <gregtandiono@gmail.com>
2013-11-12 14:03:00 -0800

    created About page template and assets

---
+++ converted/contents/about/index.md
@@ -0,0 +1,6 @@
+---
+title: Work | Winston Huxley
+template: about.jade
+description: The Portfolio of Winston Huxley
+keywords: "Gregory Tandiono, Web Developer, Web Designer, Node Developer, Ruby Developer, Ruby on Rails Developer, PHP Developer."
+---
---
+++ converted/contents/js/vexApp.js
@@ -0,0 +1,5 @@
+$('.theContent').click(function(){
+vex.open({
+content:'<div class="uk-container uk-container-center"><img src="/img/logo-badge.svg"><br><br><br><a href="/work/"l>Work</a><br><br><br><a href="">Services</a><br><br><br><a href="/about/">About</a><br><br><br><a href="">Contact</a><br><br><br></div>',
+});
+});
