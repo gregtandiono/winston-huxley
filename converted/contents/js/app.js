64d7c09a2891f62c7e698e49a01859a31809687d
gregtandiono <gregtandiono@gmail.com>
2013-11-12 01:55:17 -0800

    changed the homepage background and realigned welcome note

--- converted/contents/js/app.js
+++ converted/contents/js/app.js
@@ -9,6 +9,6 @@
 
 $('.theContent').click(function(){
 vex.open({
-content:'<div class="uk-container uk-container-center"><img src="img/logo-badge.svg"><br><br><br><a href="">Work</a><br><br><br><a href="">Services</a><br><br><br><a href="">About</a><br><br><br><a href="">Contact</a><br><br><br></div>',
+content:'<div class="uk-container uk-container-center"><img src="img/logo-badge.svg"><br><br><br><a href="/work/"l>Work</a><br><br><br><a href="">Services</a><br><br><br><a href="">About</a><br><br><br><a href="">Contact</a><br><br><br></div>',
 });
 });
